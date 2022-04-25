93b935b04c512077c877ea73937b94bf5f3b8f4f
jiyeonghwang <gyp0288@naver.com>
2022-04-25 21:18:29 +0900

    feat: 라운드 횟수 입력 메소드 추가

--- src/main/java/racingcar/utils/CarGame.java
+++ src/main/java/racingcar/utils/CarGame.java
@@ -1,19 +1,30 @@
 package racingcar.utils;
 
 import racingcar.domain.Cars;
+import racingcar.domain.Round;
 import racingcar.view.InputView;
 
 public class CarGame {
     public void start() {
         Cars cars = new Cars();
         inputCarNameList(cars);
+        Round round = new Round();
+        inputRoundCount(round);
     }
 
-    public void inputCarNameList (Cars cars){
+    public void inputCarNameList(Cars cars) {
         try {
             cars.generateCars(InputView.printCarNameRequest());
         } catch (IllegalArgumentException e) {
             inputCarNameList(cars);
+        }
+    }
+
+    public void inputRoundCount(Round round) {
+        try {
+            round.generateRound(InputView.printCountRequest());
+        } catch (IllegalArgumentException e) {
+            inputRoundCount(round);
         }
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,10 +3,16 @@ package racingcar.view;
 public class OutputView {
     private static final String ERROR = "[ERROR]";
     private static final String INPUT_ERROR = "자동차 이름은 5글자 이하로 입력하여 주시기 바랍니다.";
+    private static final String COUNT_INPUT_ERROR = "숫자로만 입력하여 주시기 바랍니다.";
 
     public static String printInputError() {
         System.out.println(ERROR + INPUT_ERROR);
         return ERROR + INPUT_ERROR;
+    }
+
+    public static String printInputCountError() {
+        System.out.println(ERROR + COUNT_INPUT_ERROR);
+        return ERROR + COUNT_INPUT_ERROR;
     }
 }
 
