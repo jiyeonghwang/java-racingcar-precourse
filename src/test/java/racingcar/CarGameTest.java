4f4b5d87f9a2566f64919ea5486c961ff321b339
jiyeonghwang <gyp0288@naver.com>
2022-04-25 21:01:06 +0900

    test: 라운드 횟수 입력 테스트 추가

--- src/test/java/racingcar/CarGameTest.java
+++ src/test/java/racingcar/CarGameTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Cars;
+import racingcar.domain.Round;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,5 +19,16 @@ class CarGameTest {
         }
         assertThat(cars.getCars().get(0).getName()).isEqualTo("car1");
         assertThat(cars.getCars().get(1).getName()).isEqualTo("car2");
+    }
+
+    @Test
+    void 라운드_횟수_입력() {
+        Round round = new Round();
+        try {
+            round.generateRound("에러");
+        } catch (IllegalArgumentException e) {
+            round.generateRound("5");
+        }
+        assertThat(round.getCount()).isEqualTo(5);
     }
 }
