29f7acd155cb39a5c130b8408c92ce07c5adc621
jiyeonghwang <gyp0288@naver.com>
2022-04-25 16:44:05 +0900

    test: 자동차 이름 입력 예외처리 테스트 추가 - empty - 5자리 이하 검증 - 중복 입력인 경우

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import racingcar.domain.Cars;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarsTest {
     @Test
@@ -14,5 +15,29 @@ public class CarsTest {
         assertThat(cars.getCars().get(1).getName()).isEqualTo("car2");
         assertThat(cars.getCars().get(2).getName()).isEqualTo("car3");
         assertThat(cars.getCars().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 자동차_이름_예외_empty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            String carNameList = ", ";
+            Cars cars = new Cars(carNameList);
+        });
+    }
+
+    @Test
+    void 자동차_이름_예외_자리수() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            String carNameList = "car,123456";
+            Cars cars = new Cars(carNameList);
+        });
+    }
+
+    @Test
+    void 자동차_이름_예외_중복() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           String carNameList = "car1,car1";
+           Cars cars = new Cars(carNameList);
+        });
     }
 }
