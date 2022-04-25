40f8efdbdc5bad65e1d9698453761483fc373957
jiyeonghwang <gyp0288@naver.com>
2022-04-25 21:32:03 +0900

    test: 라운드 진행 테스트 추가

--- src/test/java/racingcar/CarGameTest.java
+++ src/test/java/racingcar/CarGameTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.Round;
 
@@ -30,5 +31,40 @@ class CarGameTest {
             round.generateRound("5");
         }
         assertThat(round.getCount()).isEqualTo(5);
+    }
+
+    @Test
+    void 라운드_진행() {
+        Cars cars = Cars_생성();
+        Round round = Round_생성();
+        int count = 0;
+        while (count < round.getCount()) {
+            라운드_회차(cars);
+            count++;
+        }
+        assertThat(cars.getCars().get(0).getScore()).isEqualTo(5);
+    }
+
+    void 라운드_회차(Cars cars) {
+        for (Car car : cars.getCars()) {
+            int value = 5;
+            car.progress(value);
+        }
+    }
+
+    Cars Cars_생성() {
+        Cars cars = new Cars();
+        String carsNameList="car1,car2";
+        cars.generateCars(carsNameList);
+
+        return cars;
+    }
+
+    Round Round_생성() {
+        String count = "5";
+        Round round = new Round();
+        round.generateRound(count);
+
+        return round;
     }
 }
