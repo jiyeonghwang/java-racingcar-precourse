4fe2a7541681ae62d714b94cf878dde1eca2f317
jiyeonghwang <gyp0288@naver.com>
2022-04-25 22:47:57 +0900

    refactor: 출력 문구 띄어쓰기 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,7 +9,7 @@ public class OutputView {
     private static final String INPUT_ERROR = "자동차 이름은 5글자 이하로 입력하여 주시기 바랍니다.";
     private static final String COUNT_INPUT_ERROR = "숫자로만 입력하여 주시기 바랍니다.";
     private static final String RESULT = "실행 결과";
-    private static final String WINNER = "최종 우승자";
+    private static final String WINNER = "최종 우승자: ";
 
     public static String printInputError() {
         System.out.println(ERROR + INPUT_ERROR);
@@ -28,7 +28,7 @@ public class OutputView {
     public static void printCarPosition(Car car) {
         StringBuilder sb = new StringBuilder();
         sb.append(car.getName());
-        sb.append(":");
+        sb.append(" : ");
         int count = 0;
         while (count < car.getScore()) {
             sb.append("-");
@@ -42,7 +42,7 @@ public class OutputView {
     }
 
     public static void printWinner(String winner) {
-        System.out.println(WINNER + ":" + winner);
+        System.out.println(WINNER + winner);
     }
 }
 
